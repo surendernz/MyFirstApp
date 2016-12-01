8dfd99cf2f30eeaed5050c050154f1e687d2ce44
wonderkid20 <shreyas.adavalli@gmail.com>
2016-12-01 22:28:00 +1300

    first Commit to github

---
+++ MyFirstIntelliJApp/src/com/surender/Main.java
@@ -0,0 +1,16 @@
+package com.surender;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    System.out.println("this is a test :"+args[0] +" "+args[1]);
+
+    MathOps ops = new MathOps();
+
+    System.out.println("Add 2,3 : " + ops.add(2,3));
+
+    System.out.println("Substract 5,3 : " + ops.add(5,3));
+
+    }
+}
---
+++ MyFirstIntelliJApp/src/com/surender/MathOps.java
@@ -0,0 +1,13 @@
+package com.surender;
+
+/**
+ * Created by Surender on 1/12/2016.
+ */
+public class MathOps {
+    public int add(int a,int b){
+        return a+b;
+    }
+    public int substract (int a,int b){
+        return a-b;
+    }
+}
